7eac9a5cab1171a1a9ff231f2a2b265988cb6aa3
MINEUNCHAE <c389074@g.hongik.ac.kr>
2025-07-04 14:33:56 +0900

    chore: frist init

--- src/main/java/com/metaverse/springprepare/domain/Memo.java
+++ src/main/java/com/metaverse/springprepare/domain/Memo.java
@@ -6,7 +6,7 @@ import lombok.*;
 @Setter //값 변경
 @NoArgsConstructor//기본 생성자 생성 애너테이션
 @AllArgsConstructor//모든 필드를 파라미터로 받는 생성자
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class Memo {
     private String username;
     private String contant;
